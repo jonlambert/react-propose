57baa16e601e1f7152403f5e4c592632e6f9cfcf
Jon Lambert <jonlambert@icloud.com>
2022-04-27 11:20:10 +0100

    feat: support passing refs to intrinsic elements

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import React, {
   createElement,
   JSXElementConstructor,
   ComponentProps,
+  forwardRef,
+  ForwardedRef,
+  RefCallback,
+  Ref,
 } from 'react';
 
 type IntrinsicElementKeys = keyof JSX.IntrinsicElements;
@@ -9,10 +13,24 @@ type IntrinsicElementKeys = keyof JSX.IntrinsicElements;
 export function propose<
   ComponentType extends JSXElementConstructor<any> | IntrinsicElementKeys,
   OriginalProps extends ComponentProps<ComponentType>,
-  SuppliedProps extends Partial<OriginalProps>
+  SuppliedProps extends Partial<OriginalProps>,
+  RefType extends ComponentType extends IntrinsicElementKeys
+    ?
+        | Ref<JSX.IntrinsicElements[ComponentType]>
+        | RefCallback<JSX.IntrinsicElements[ComponentType]>
+    : "Unable to pass a ref to a function component"
 >(Component: ComponentType, props: SuppliedProps, displayName?: string) {
   type FinalProps = Omit<OriginalProps, keyof SuppliedProps> &
     Partial<SuppliedProps>;
+
+  if (typeof Component === 'string') {
+    const NewComponent = forwardRef<RefType, FinalProps>((p, ref) => {
+      const combinedProps = { ...props, ...p, ref };
+      return createElement(Component, combinedProps);
+    });
+    NewComponent.displayName = displayName;
+    return NewComponent;
+  }
 
   const NewComponent: React.FC<FinalProps> = (p) => {
     const combinedProps = { ...props, ...p };
